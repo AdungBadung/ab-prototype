373ae9e4120794761b1a913194f77ff74099e247
proocoss <lloyd.lee@kakaoenterprise.com>
2022-05-02 21:07:30 +0900

    [feature/issue-44] craco alias 설치 및 설정

--- craco.config.js
+++ craco.config.js
@@ -1,1 +1,17 @@
-module.exports = { babel: { presets: ['@emotion/babel-preset-css-prop'] } };
+const CracoAlias = require('craco-alias');
+
+module.exports = {
+  babel: {
+    presets: ['@emotion/babel-preset-css-prop']
+  },
+  plugin: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: './src',
+        tsConfigPath: './tsconfig.extend.json'
+      }
+    }
+  ]
+};
