b72aaf82c85f2bcc3644a459e91a712223f859ea
proocoss <lloyd.lee@kakaoenterprise.com>
2022-05-08 18:07:07 +0900

    [feature/issue-44] app 폴더 구조 추가

--- src/layouts/GlobalLayout.tsx
+++ src/layouts/GlobalLayout.tsx
@@ -1,4 +1,6 @@
 import { Outlet } from 'react-router-dom';
+import GlobalNavigation from '@app/common/components/GlobalNavigation';
+
 const GlobalLayout = () => {
   /**
    * TODO Global 하게 검색 등의 이동으로 패스가 변경되어도 계속 유지가 될 컴포넌트를 추가
@@ -6,6 +8,7 @@ const GlobalLayout = () => {
    */
   return (
     <>
+      <GlobalNavigation />
       <Outlet />
     </>
   );
--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,4 +1,6 @@
 import { Outlet } from 'react-router-dom';
+import SearchBar from '@app/home/components/SearchBar';
+
 const HomeLayout = () => {
   /**
    * TODO Home (search page) 에서 검색 등의 이동으로 패스가 변경되어도 계속 유지가 될 컴포넌트를 추가
@@ -6,6 +8,7 @@ const HomeLayout = () => {
    */
   return (
     <>
+      <SearchBar />
       <Outlet />
     </>
   );
