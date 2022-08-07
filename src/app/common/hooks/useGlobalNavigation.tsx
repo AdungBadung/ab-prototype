75f3224e39340c475ff7b902f2196d362792147a
hoonsgood <hoonsgood@gmail.com>
2022-08-07 21:24:18 +0900

    Feature/issue 55 (#56)
    * update readme
    
    * [feature/issue-55] GlobalNavigation 컴포넌트 구현
    
    Co-authored-by: Taehoon Kim <taehoon-a.kim@rohde-schwarz.com>

--- src/app/common/hooks/useGlobalNavigation.tsx
+++ src/app/common/hooks/useGlobalNavigation.tsx
@@ -1,5 +1,26 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const useGlobalNavigation = () => {
-  return { dummy: true };
+  const navigate = useNavigate();
+  const [selectedTab, setSelectedTab] = useState(0);
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    switch (newValue) {
+      case 0:
+        navigate('/search');
+        break;
+      case 1:
+        navigate('/for-you');
+        break;
+      case 2:
+        navigate('/my');
+        break;
+      default:
+        break;
+    }
+    setSelectedTab(newValue);
+  };
+  return { selectedTab, handleChange };
 };
 
 export default useGlobalNavigation;
